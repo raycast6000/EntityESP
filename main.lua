d9be915ebf3b80e24621fa26af087ca49c3783c8
Raycast <raycast6000@gmail.com>
2023-03-15 18:02:48 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,123 +1,49 @@
-local Vec2 = Vector2.new
-local Col3 = Color3.new
-local floor = math.floor
-
--- // VFX //
-local TweenService = game:GetService("TweenService")
-local In = Enum.EasingDirection.In
-local Quint = Enum.EasingStyle.Quint
-
--- // Builders //
-local TrBuilder = loadstring(readfile('EntityESP/builders/tracer.lua'))()
-local BxBuilder = loadstring(readfile('EntityESP/builders/box.lua'))()
-local TxtBuilder = loadstring(readfile('EntityESP/builders/text.lua'))()
-
--- // Player //
 local Players = game:GetService("Players")
 local Player = Players.LocalPlayer
-local Character = Player.Character or Player.CharacterAdded:Wait()
-local Camera = workspace.CurrentCamera
-local ScreenCenterX, ScreenCenterY = Camera.ViewportSize.X / 2, Camera.ViewportSize.Y / 2
-
--- // Render //
 local RunService = game:GetService("RunService")
-local RenderStepped = RunService.RenderStepped
-local Heartbeat = RunService.Heartbeat
+local Camera = workspace.CurrentCamera
 
--- // Whitelist //
-local whitelist = {}
+function Draw(entity: Model, line)
+    local vector, screen = Camera:WorldToViewportPoint(entity.HumanoidRootPart.Position)
 
-function checkExists(plr)
-    local found = false
-    
-    for k,v in pairs(whitelist) do
-        if v.player == plr then
-            found = true
-            break
-        end
-    end
-
-    if found then
-        return true
+    if screen then
+        line.Visible = true
+        line.To = Vector2.new(vector.X, vector.Y)
     else
-        return false
+        line.Visible = false
     end
 end
 
-function hasHead(i)
-    return i:FindFirstChild("Head")
-end
+function Link(entity: Model)
+    local Line = Drawing.new("Line")
+    Line.From = Vector2.new(Camera.ViewportSize.X / 2, Camera.ViewportSize.Y / 2)
+    Line.To = Vector2.new(0, 0)
+    Line.Color = Color3.fromRGB(255, 255, 255)
+    Line.Visible = true
+    Line.Thickness = 2
 
-function UpdateRender(tracer, tI, plr)
-    local vector, onScreen = Camera:WorldToViewportPoint(plr.Head.Position)
-
-    if onScreen and checkExists(plr) then
-        tracer.Visible = true
-        tI.Visible = true
-        tI.Text = plr.Name .. " (" .. floor((Player.Character.HumanoidRootPart.Position - plr.HumanoidRootPart.Position).Magnitude) .. " studs)"
-        tracer.To = Vec2(vector.X, vector.Y)
-        tI.Position = Vec2(vector.X, vector.Y - 25)
-    else
-        tracer.Visible = false
-        tI.Visible = false
-    end
-end
-
-workspace.ChildRemoved:Connect(function (c)
-    for k,v in pairs(whitelist) do
-        if v.player == c then
-            v.tracer:Remove()
-            v.tInfo:Remove()
-            table.remove(whitelist, k)
-        end
-    end
-end)
-
-workspace.ChildAdded:Connect(function (v)
-    delay(1, function ()
-        if v:IsA("Model") and v:FindFirstChild("Humanoid") and v ~= Player.Character then
-            print("Assigning a tracer to", v.Name)
-    
-            if hasHead(v) then
-                local targetInfo = TxtBuilder.new(v.Name)
-                targetInfo.Color = Col3(255,255,255)
-        
-                local Tracer = TrBuilder.new()
-                Tracer.From = Vec2(ScreenCenterX, ScreenCenterY)
-                Tracer.To = Vec2(0, 0)
-    
-                table.insert(whitelist, {player = v, tracer = Tracer, tInfo = targetInfo})
-    
-                local PlrRenderCycle = RenderStepped:Connect(function ()
-                    if v ~= Character and checkExists(v) and hasHead(v) then
-                        UpdateRender(Tracer, targetInfo, v)
-                    end
-                end)
-    
-            end
+    local Render = RunService.RenderStepped:Connect(function()
+        if entity:FindFirstChild("HumanoidRootPart") ~= nil then
+            Draw(entity, Line) 
         end
     end)
-end)
+
+    workspace.ChildRemoved:Connect(function(child)
+        if child == entity then
+            Render:Disconnect()
+            Line:Remove()
+        end
+    end)
+end
 
 for _,v in pairs(workspace:GetChildren()) do
-    if v:isA("Model") and v:FindFirstChild("Humanoid") and v ~= Player.Character then
-
-        if hasHead(v) then
-            local targetInfo = TxtBuilder.new(v.Name)
-            targetInfo.Color = Col3(255,255,255)
-    
-            local Tracer = TrBuilder.new()
-            Tracer.From = Vec2(ScreenCenterX, ScreenCenterY)
-            Tracer.To = Vec2(0, 0)
-
-            table.insert(whitelist, {player = v, tracer = Tracer, tInfo = targetInfo})
-
-            local PlrRenderCycle = RenderStepped:Connect(function ()
-                if v ~= Character and checkExists(v) and hasHead(v) then
-                    UpdateRender(Tracer, targetInfo, v)
-                end
-            end)
-
-        end
+    if v ~= Player.Character and v:isA("Model") and v:FindFirstChildOfClass("Humanoid") and v:FindFirstChild("HumanoidRootPart") then
+        Link(v)
     end
 end
+
+workspace.ChildAdded:Connect(function(child)
+    if child ~= Player.Character and child:isA("Model") and child:FindFirstChildOfClass("Humanoid") and child:FindFirstChild("HumanoidRootPart") then
+        Link(child)
+    end
+end)
